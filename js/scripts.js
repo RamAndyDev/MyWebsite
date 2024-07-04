2acb8299c873dffc5cc12107e28f098dd7b10001
Ramprasad G <mail2ram.andydev@gmail.com>
2024-07-04 20:55:28 +0530

    Converted to single page website.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('JavaScript Loaded');
+    const links = document.querySelectorAll('nav ul li a');
+    for (const link of links) {
+        link.addEventListener('click', smoothScroll);
+    }
 });
+
+function smoothScroll(event) {
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    window.scrollTo({
+        top: targetElement.offsetTop - 60, // Adjust for fixed header height
+        behavior: 'smooth'
+    });
+}
